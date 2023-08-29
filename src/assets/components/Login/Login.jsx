11dcf096dc49ef4e837691933002864169921d05
Rahat73 <rahat.ashik.18@gmail.com>
2023-08-29 21:58:50 +0600

    login signup page updated

--- src/assets/components/Login/Login.jsx
+++ src/assets/components/Login/Login.jsx
@@ -1,7 +1,28 @@
 // import React from "react";
+import "./Login.css";
 
 const Login = () => {
-  return <div>This is login page.</div>;
+  return (
+    <div className="form-container">
+      <h1 className="form-title">Login</h1>
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
+      </form>
+      <div className="divider">
+        <hr className="line" />
+        <span>or</span>
+        <hr className="line" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/assets/components/SignUp/SignUp.jsx
+++ src/assets/components/SignUp/SignUp.jsx
@@ -2,7 +2,31 @@
 import "./SignUp.css";
 
 const SignUp = () => {
-  return <div>Welcome to sign up</div>;
+  return (
+    <div className="form-container">
+      <h1 className="form-title">Sign Up</h1>
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirm">Confirm password</label>
+          <input type="password" name="confirm" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Sign Up" />
+      </form>
+      <div className="divider">
+        <hr className="line" />
+        <span>or</span>
+        <hr className="line" />
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
